acdb50cb79b3eda9700951fdff75735e39b982ad
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-11-21 12:52:22 +0100

    creates MoviesCarousel component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css"
 
 import MyNavbar from "./components/MyNavbar"
 import Header from "./components/Header"
+import MoviesCarousel from "./components/MoviesCarousel"
 
 function App() {
   return (
     <>
       <MyNavbar />
       <Header />
+      <MoviesCarousel title="Film sugli Avengers" query="avengers" />
+      <MoviesCarousel title="Film su Batman" query="batman" />
+      <MoviesCarousel title="Film su Harry Potter" query="harry potter" />
     </>
   )
 }
---
+++ src/components/MoviesCarousel.jsx
@@ -0,0 +1,95 @@
+import { Component } from "react"
+import Carousel from "react-bootstrap/Carousel"
+import { Container, Row, Col } from "react-bootstrap"
+
+const API_KEY = "6772201a"
+
+class TrendingCarousel extends Component {
+  state = {
+    movies: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.fetchMovies()
+  }
+
+  fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?s=${this.props.query}&apikey=${API_KEY}`
+      )
+
+      const data = await response.json()
+
+      this.setState({
+        movies: (data.Search || []).slice(0, 9),
+        isLoading: false,
+      })
+    } catch (error) {
+      console.error("Errore fetch:", error)
+      this.setState({ isLoading: false })
+    }
+  }
+
+  chunkArray = (array, size) => {
+    const result = []
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size))
+    }
+    return result
+  }
+
+  render() {
+    const { movies, isLoading } = this.state
+
+    if (isLoading) return <p className="text-light">Caricamento...</p>
+
+    const validMovies = movies.filter(
+      (movie) => movie.Poster && movie.Poster !== "N/A"
+    )
+
+    const slides = this.chunkArray(validMovies, 3)
+
+    return (
+      <>
+        <Container className="px-4 my-4">
+          <h3 className="text-light">{this.props.title}</h3>
+
+          <Carousel className="px-5 my-5">
+            {slides.map((group, index) => (
+              <Carousel.Item key={index}>
+                <Row className="g-2">
+                  {group.map((movie) => (
+                    <Col
+                      key={movie.imdbID}
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      className={
+                        group.indexOf(movie) === 1
+                          ? "d-none d-sm-block"
+                          : group.indexOf(movie) === 2
+                          ? "d-none d-md-block"
+                          : ""
+                      }
+                    >
+                      <img
+                        src={movie.Poster}
+                        className="d-block w-100 rounded"
+                        alt={movie.Title}
+                        style={{ height: "18rem", objectFit: "cover" }}
+                      />
+                    </Col>
+                  ))}
+                </Row>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Container>
+      </>
+    )
+  }
+}
+
+export default TrendingCarousel
